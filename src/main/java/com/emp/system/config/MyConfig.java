018e0f383159da3665ca2369c90215ba81ee5bae
AvinashChaturvedi <avinashchaturvedi2023@gmail.com>
2024-05-31 12:20:46 +0530

    Exception added.

--- src/main/java/com/emp/system/config/MyConfig.java
+++ src/main/java/com/emp/system/config/MyConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
